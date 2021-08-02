ab4079645f5c8133684d4622b68e8786d9d01f09
Raul Trejo <rdanieltrejoguzman@gmail.com>
2021-08-02 02:17:30 -0700

    This Market Is Crazy

---
+++ script.js
@@ -0,0 +1,18 @@
+$("li").on("click", function() {
+  alert("These links don't work, silly!");
+});
+function myFunction() {
+alert("2020 ISN'T OVER YET!");
+}
+function moneyprinter() {
+alert("No, Silly, Money Printer Go: Brrrrrr!");
+}
+function bullmarket() {
+alert("Only if you hate money.");
+}
+function secret() {
+alert("42.");
+}
+function submit(){
+alert("Thank you for subscribing to my newsletter!");
+}
